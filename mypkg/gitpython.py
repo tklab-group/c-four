8083d3437388ce407194aa731ab18d37c457507f
junior <bluehawaiiouhu@gmail.com>
2021-10-17 18:15:47 +0900

    fix get_repo method to work

--- mypkg/gitpython.py
+++ mypkg/gitpython.py
@@ -2,9 +2,5 @@ import os
 import git
 from git import Repo
 
-path = os.getcwd()
-repo = Repo(path)
-print(repo.untracked_files)
-
 def get_repo():
-    return Repo(os.getcwd)
+    return Repo(os.getcwd())
