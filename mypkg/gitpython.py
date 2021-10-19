615e4decd21ebd982596a3f09f451d12a158ee41
junior <bluehawaiiouhu@gmail.com>
2021-10-19 12:11:51 +0900

    add some methods to gitpython

--- mypkg/gitpython.py
+++ mypkg/gitpython.py
@@ -1,6 +1,27 @@
-import os
 import git
 from git import Repo
+import textwrap
+import tempfile
 
-def get_repo():
-    return Repo(os.getcwd())
+def get_repo(path):
+    return Repo(path)
+
+def get_diffs(repo):
+    return repo.head.commit.diff(None, create_patch=True).iter_change_type('M')
+
+def make_patch(file, diff):
+    patch = textwrap.dedent("""\
+    diff --git a/{0} b/{0}
+    --- a/{0}
+    +++ b/{0}
+    """.format(file)) + diff
+
+    return patch
+
+def auto_commit(repo, patches):
+    for index, patch in enumerate(patches):
+        with tempfile.NamedTemporaryFile(mode='w+') as tf:
+            tf.write(patch)
+            tf.seek(0)
+            repo.git.apply(['--cached', tf.name])
+            repo.git.commit('-m', 'commit: {0}'.format(index))
