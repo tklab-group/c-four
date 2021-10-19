0d6019253359391ff4d02faa6cca8231006c01e2
junior <bluehawaiiouhu@gmail.com>
2021-10-19 12:11:26 +0900

    refactor parse_diff

--- mypkg/parse_diff.py
+++ mypkg/parse_diff.py
@@ -7,50 +7,56 @@ def main():
     print("added_chunks: {0}".format(added_chunks))
     print("removed_chunks: {0}".format(removed_chunks))
 
-def parse_diff(diff_str):
-    print("diff_str: {0}".format(diff_str))
-    # lines = diff_str.split('\n')
-    lines = diff_str[4:]
-    line_iterator = iter(lines)
-    added_chunks = []
-    removed_chunks = []
+def parse_diff(lines):
+    lines = lines.split('\n')
+    added_chunks, removed_chunks = [], []
+    added_line_ids, removed_line_ids = [], []
+    added_lines, removed_lines = [], []
     regexp = re.compile(',| ')
+    
+    first_line = lines.pop(0)
+    if first_line.startswith('@@'):
+        line_id_strs = [token[1:] for token in regexp.split(first_line.split('@@')[1]) if token.startswith(('+', '-'))]
+        removed_line_id = int(line_id_strs[0])
+        added_line_id = int(line_id_strs[1])
+    else:
+        return
 
-    try:
-        line = next(line_iterator)
-        while True:
-            if line.startswith('@@'):
-                line_id_strs = [token[1:] for token in regexp.split(line.split('@@')[1]) if token.startswith(('+', '-'))]
-                removed_line_id = int(line_id_strs[0])
-                added_line_id = int(line_id_strs[1])
-                line = next(line_iterator)
-            elif line.startswith('+'):
-                chunk_start = added_line_id
-                try:
-                    while line.startswith('+'):
-                        line = next(line_iterator)
-                        added_line_id += 1
-                except StopIteration:
-                    added_chunks.append((chunk_start, added_line_id))
-                    break
-                added_chunks.append((chunk_start, added_line_id - 1))
-            elif line.startswith('-'):
-                chunk_start = removed_line_id
-                try:
-                    while line.startswith('-'):
-                        line = next(line_iterator)
-                        removed_line_id += 1
-                except StopIteration:
-                    removed_chunks.append((chunk_start, removed_line_id))
-                    break
-                removed_chunks.append((chunk_start, removed_line_id - 1))
-            else:
-                line = next(line_iterator)
-                added_line_id += 1
-                removed_line_id += 1
+    for line in lines:
+        if line.startswith('+'):
+            added_line_ids.append(added_line_id)
+            added_line_id += 1
+        elif line.startswith('-'):
+            removed_line_ids.append(removed_line_id)
+            removed_line_id += 1
+        else:
+            added_line_id += 1
+            removed_line_id += 1
 
-    except StopIteration:
-        pass
+    print(added_line_ids)
+    print(removed_line_ids)
+    
+    start_id = added_line_ids.pop(0)
+    prev_id = end_id = start_id
+    added_line_ids.append(-1)
+    for id in added_line_ids:
+        if id == prev_id + 1:
+            end_id = id
+        else:
+            added_chunks.append((start_id, end_id))
+            start_id = end_id = id
+        prev_id = id
+
+    start_id = removed_line_ids.pop(0)
+    prev_id = end_id = start_id
+    removed_line_ids.append(-1)
+    for id in removed_line_ids:
+        if id == prev_id + 1:
+            end_id = id
+        else:
+            removed_chunks.append((start_id, end_id))
+            start_id = end_id = id
+        prev_id = id
 
     return (added_chunks, removed_chunks)
 
