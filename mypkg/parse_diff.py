5cac183e755759f61b9e6acc0cec744f806af073
junior <bluehawaiiouhu@gmail.com>
2021-10-20 18:28:49 +0900

    commit: /tmp/tmp490ze5v1

--- mypkg/parse_diff.py
+++ mypkg/parse_diff.py
@@ -83,6 +83,10 @@ class Context:
         
         split_add_chunk(add_line_infos, self.add_chunks)
         split_remove_chunk(remove_line_ids, self.remove_chunks)
+        if bool(add_line_infos):
+            split_add_chunk(add_line_infos, self.add_chunks)
+        if bool(remove_line_ids):
+            split_remove_chunk(remove_line_ids, self.remove_chunks)
     
     def make_add_patch(self, chunk):
         start_id, end_id = chunk.start_id, chunk.end_id
