185b032d737665c421a8c5b54002491e10cb28f3
junior <bluehawaiiouhu@gmail.com>
2021-10-20 19:42:17 +0900

    mypkg/script.py: (23,24)

--- mypkg/script.py
+++ mypkg/script.py
@@ -20,8 +20,6 @@ def main():
             operate_git.auto_commit(repo, patch, diff.a_path, ac.start_id, ac.end_id)
 
         for rc in context.remove_chunks:
-            patch_code = context.make_remove_patch(rc)
-            patch = gitpython.make_patch(diff.a_path, patch_code)
             patch_content = context.make_remove_patch_content(rc)
             patch = make_patch.make_full_patch(diff.a_path, patch_content)
             print(patch)
