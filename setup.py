21631e92b8ecf81d1df4dbbc5134198a3ecd4927
junior <bluehawaiiouhu@gmail.com>
2022-01-06 20:58:15 +0900

    rename project

--- setup.py
+++ setup.py
@@ -4,20 +4,20 @@ with open('requirements.txt') as requirements_file:
     install_requirements = requirements_file.read().splitlines()
 
 console_scripts = [
-    'auto_split_commits=auto_split_commits.script:main',
+    'c-four=c-four.script:main',
 ]
 
 setup(
-    name="auto_split_commits",
+    name="c-four",
     version='1.0',
     description='コミットの自動分割ツール',
     author='Aoi Koga',
     author_email='bluehawaiiouhu@gmail.com',
-    url='https://github.com/juniortwelve/auto_split_commits',
+    url='https://github.com/juniortwelve/c-four',
     packages=find_packages(),
     entry_points={
         "console_scripts": [
-            "auto_split_commits=mypkg.script:main",
+            "c-four=mypkg.script:main",
         ]
     },
 )
