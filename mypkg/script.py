c59c5218dc4dbaa148f743cb1043e47d1f4eaa4d
junior <bluehawaiiouhu@gmail.com>
2022-01-18 16:42:50 +0900

    apply json input

--- mypkg/script.py
+++ mypkg/script.py
@@ -8,6 +8,8 @@ from mypkg.models.remove_chunk import RemoveChunk
 from mypkg.models.chunk_set import ChunkSet
 from mypkg.prompts.main_prompt import generate_main_screen
 from prompt_toolkit.shortcuts import yes_no_dialog
+from mypkg.operate_json import make_file_unit_json, convert_json_to_data
+import json
 
 def main():
     path = os.getcwd()
@@ -15,12 +17,19 @@ def main():
     diffs = operate_git.get_diffs(repo)
     Base.metadata.create_all(engine)
     
-    for diff in diffs:
-        context = Context(diff.a_path)
-        session.add(context)
-        session.commit()
-        context.convert_diff_to_chunk(diff.diff.decode())
+    file_unit_json = make_file_unit_json(diffs)
+    with open('./json/sample.json', 'w') as f:
+        json.dump(file_unit_json, f, ensure_ascii=False, indent=2)
+    
+    # return None
+    
+    # for diff in diffs:
+    #     context = Context(diff.a_path)
+    #     session.add(context)
+    #     session.commit()
+    #     context.convert_diff_to_chunk(diff.diff.decode())
 
+    convert_json_to_data(file_unit_json)
     while True:
         all_chunks = []
         all_add_chunks = AddChunk.query.all()
@@ -28,7 +37,7 @@ def main():
         all_chunks.extend(all_add_chunks)
         all_chunks.extend(all_remove_chunks)
         
-        split_chunks_by_file(all_chunks)
+        # split_chunks_by_file(all_chunks)
         chunk_sets = ChunkSet.query.all()
         
         cur_chunk_set_idx = 0
@@ -42,7 +51,8 @@ def main():
             cur_chunks.extend(cur_chunk_set.remove_chunks)
             path_sets = {chunk.context.path for chunk in cur_chunks}
             
-            related_chunks = [chunk for chunk in all_chunks if chunk.context.path in path_sets and chunk.chunk_set_id != cur_chunk_set.id]
+            # related_chunks = [chunk for chunk in all_chunks if chunk.context.path in path_sets and chunk.chunk_set_id != cur_chunk_set.id]
+            related_chunks = []
             pending_chunks = [chunk for chunk in all_chunks if chunk.chunk_set_id is None]
             related_chunks.extend(pending_chunks)
             related_chunks = list(set(related_chunks))
