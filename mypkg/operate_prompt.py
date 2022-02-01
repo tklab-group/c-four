5b0a3b3efb38a534ea9bd3ec935a8d44cc4f275e
junior <bluehawaiiouhu@gmail.com>
2022-02-01 13:59:18 +0900

    fix confirm dialog

--- mypkg/operate_prompt.py
+++ mypkg/operate_prompt.py
@@ -44,39 +44,20 @@ def run_prompt(repo, log_path):
                 if cur_chunk_set_idx == len(chunk_sets):
                     cur_chunk_set_idx -= 1
         
+        pending_chunk_count = AddChunk.query.filter(AddChunk.chunk_set_id == None).count() + RemoveChunk.query.filter(RemoveChunk.chunk_set_id == None).count()
+        if pending_chunk_count:
+            result = yes_no_dialog(
+                title="Confirm making commits",
+                text="There is still {} pending chunks.\nDo you really want to make commits?".format(pending_chunk_count)
+            ).run()
+    
+            if not result:
+                cur_chunk_set_idx -= 1
+                continue
+        
         construct_json_from_data(log_path)
         # stage and commit current chunk sets
         for chunk_set in chunk_sets:
             chunk_set.commit_self_chunks(repo)
-        
-        for chunk_set in chunk_sets:
-            for add_chunk in chunk_set.add_chunks:
-                AddChunk.query.filter(AddChunk.id == add_chunk.id).delete()
-            session.commit()
-            for remove_chunk in chunk_set.remove_chunks:
-                RemoveChunk.query.filter(RemoveChunk.id == remove_chunk.id).delete()
-            session.commit()
-            ChunkSet.query.filter(ChunkSet.id == chunk_set.id).delete()
-            session.commit()
-        
-        other_chunks = []
-        other_chunks.extend(AddChunk.query.all())
-        other_chunks.extend(RemoveChunk.query.all())
-        
-        if other_chunks:
-            result = yes_no_dialog(
-                title="Confirm staging of pending chunks",
-                text="There is still {} pending chunks.\nDo you want to stage these chunks?".format(len(other_chunks))
-            ).run()
             
-            if result:
-                new_chunk_set = ChunkSet()
-                session.add(new_chunk_set)
-                session.commit()
-                for other_chunk in other_chunks:
-                    other_chunk.chunk_set_id = new_chunk_set.id
-                session.commit()
-            else:
-                break
-        else:
-            break
+        break
